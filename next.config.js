8666931cedc2e9eb42b393413ba2a5e6d19bfbc7
mirzavic4869 <avicenamirzam@gmail.com>
2023-05-08 10:25:24 +0700

    get all digimon

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "digimon.shadowsmith.com",
+				port: "",
+				pathname: "/img/**",
+			},
+		],
+	},
+};
